89e6d399334d65c94d4fcb0ff9371bde8fac02fb
jaeyow <jaeyow@gmail.com>
2024-02-21 23:15:15 +1100

    start updating for github post

--- app/app.py
+++ app/app.py
@@ -1,8 +1,7 @@
-import numpy as np
-from app.algorithm.kohonen import Kohonen
-from PIL import Image
 import io
-
+from app.algorithm.kohonen import Kohonen
+import numpy as np
+from PIL import Image
 from fastapi import FastAPI, Response
 
 app = FastAPI()
@@ -11,32 +10,41 @@ app = FastAPI()
 @app.get(
     "/kohonen", responses={200: {"content": {"image/png": {}}}}, response_class=Response
 )
-async def read_root():
-    kohonen_som = Kohonen(
-        None, random=True, input_size=20, width=100, height=100, max_iterations=1000
-    )
+async def kohonen():
+    """
+    Kohonen Self-Organizing Map (SOM) algorithm
+    """
+    try:
+        kohonen_som = Kohonen(
+            None, random=True, input_size=20, width=100, height=100, max_iterations=1000
+        )
 
-    kohonen_som.fit()
+        kohonen_som.fit()
 
-    image_input_layer = Image.fromarray(
-        kohonen_som.get_input_layer().vectors.astype(np.uint8)
-    ).resize((400, 20), resample=Image.Resampling.NEAREST)
+        image_input_layer = Image.fromarray(
+            kohonen_som.get_input_layer().vectors.astype(np.uint8)
+        ).resize((400, 20), resample=Image.Resampling.NEAREST)
 
-    image_node_map = Image.fromarray(
-        kohonen_som.get_output_layer().nodes.astype(np.uint8)
-    ).resize((400, 400))
+        image_node_map = Image.fromarray(
+            kohonen_som.get_output_layer().nodes.astype(np.uint8)
+        ).resize((400, 400))
 
-    concatenated_image = Image.new(
-        "RGB",
-        (image_input_layer.width, image_input_layer.height + image_node_map.height),
-    )
-    concatenated_image.paste(image_input_layer, (0, 0))
-    concatenated_image.paste(image_node_map, (0, image_input_layer.height))
+        concatenated_image = Image.new(
+            "RGB",
+            (image_input_layer.width, image_input_layer.height + image_node_map.height),
+        )
+        concatenated_image.paste(image_input_layer, (0, 0))
+        concatenated_image.paste(image_node_map, (0, image_input_layer.height))
 
-    with io.BytesIO() as buf:
-        concatenated_image.save(buf, format="PNG")
-        im_concatenated_bytes = buf.getvalue()
+        with io.BytesIO() as buf:
+            concatenated_image.save(buf, format="PNG")
+            im_concatenated_bytes = buf.getvalue()
 
-    headers = {"Content-Disposition": 'inline; filename="kohonen_som_input_output.png"'}
+        headers = {
+            "Content-Disposition": 'inline; filename="kohonen_som_input_output.png"'
+        }
+        return Response(im_concatenated_bytes, headers=headers, media_type="image/png")
 
-    return Response(im_concatenated_bytes, headers=headers, media_type="image/png")
+    except Exception as e:
+        error_message = f"An error occurred: {str(e)}"
+        return Response(error_message, status_code=500)
